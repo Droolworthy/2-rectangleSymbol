acf2a4704060f7be9a9f9f4d2494bd320e8ac961
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-25 14:42:45 +0300

    Добавьте файлы проекта.

---
+++ CS14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CS14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+            int namelenght;
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            namelenght = name.Length;
+            Console.Write("Введите символ: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+            Console.Write("\n");
+
+            for(int i = 0; i < namelenght + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine("\n" + symbol + name + symbol);
+
+            for (int i = 0; i < namelenght + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.ReadKey();
+        }
+    }
+}
